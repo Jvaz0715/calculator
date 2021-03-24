54df2c5b7a26a392658f426dd30b470979b67e6a
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-24 15:10:50 -0400

    Deletes old code. Recode using action sets and event targets

--- main.js
+++ main.js
@@ -1,69 +1,27 @@
-// One approach is to store our two numbers and one operation as strings so that 
-// we can easily add characters as the user presses buttons
-let firstNumber = ''
-let operation = '';
-let secondNumber = '';
-// all of numbers buttons
-const numberButtons = document.querySelectorAll(".number");// this is good don't touch
-//add event listeners to each number
-for (const number of numberButtons) {
-    number.addEventListener('click', function (){
-        display.innerHTML = number.innerHTML
-    })
-}
+const calculator = document.querySelector("#calculator-container");
 
+const buttons = calculator.querySelector(".buttons-container");
 
-//all of the operations
-const operatorButtons = document.querySelectorAll(".operator");
-//add event listener to each operator
-for (const operator of operatorButtons) {
-    operator.addEventListener('click', function (){
-        display.innerHTML = operator.innerHTML;
-    })
-}
+buttons.addEventListener('click', event => {
+    if (event.target.matches('button')) {
+        //do something
+        //console.log('something was clicked')
 
-//the equal sign
-const equals = document.querySelector(".equals");
+        const button = event.target;
+        const action = button.dataset.action;
+        // checks if button pressed is number
+        if (!action) {
+            console.log('You pressed a number');
+        }
+        //checks if button is an operator
+        if (
+            action === "add" ||
+            action === "subtract" ||
+            action === "multiply" ||
+            action === "divide"
+        ) {
+            console.log("you pressed an operator key")
+        }
 
-// the decimal button
-const decimal = document.querySelector(".decimal");
-
-//the clear button
-const clear = document.querySelector(".clear");
-clear.addEventListener('click', function() {
-    display.innerHTML = '';
+    }
 })
-
-// grabs display
-const display = document.querySelector("#display-content");
-
-
-
-
-// Calculates the result of the current expression if it is valid, then displays the result on the screen
-function calcResult() {
-  // TODO
-}
-
-// Handles when an operation button is pressed (+, -, /, *)
-function operationPressed(op) {
-  // TODO
-}
-
-// Handles when a number button is pressed
-function numberPressed(number) {
-  // TODO
-}
-
-// Clears the screen
-function clearScreen() {
-  // TODO
-}
-
-// Updates the screen based on `firstNumber`, `operation`, and `secondNumber`
-function updateScreen() {
-  // TODO
-}
-
-// TODO: write query selectors and add event listeners to the calculator's buttons
-
